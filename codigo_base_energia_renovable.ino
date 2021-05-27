160170c511a58fc6d2899262e9b9fca2ba9af8b2
BrayanRivas <83795089+BrayanRivas@users.noreply.github.com>
2021-05-26 20:47:01 -0600

    Update codigo_base_energia_renovable.ino
    Se manipulan los ángulos del servo como actuador a la señal análoga del las fotorresistencias, la condicional if para volver a la posicion original(linea 36) dependerá del  el actuador del motor paso a paso.

--- codigo_base_energia_renovable.ino
+++ codigo_base_energia_renovable.ino
@@ -1,28 +1,39 @@
+#include <Servo.h>
+//variable para manipular el servo
+Servo servoMotor;
 //asignando A0 a constante photA(fotorresistencia A)
 #define photA A0
 //asignando A0 a constante photB(fotorresistencia B)
 #define photB A1
+//Variable para activar el servo por los valores de las ldr
+const int activarServo = 200;
+
 void setup()
 {
   Serial.begin(9600);
   //Definiendo pines de entrada y salida
   pinMode(photA, INPUT);
   pinMode(photB, INPUT);
+  // Iniciamos el servo para que empiece a trabajar con el pin asignado
+  servoMotor.attach(8);
 }
-
 void loop()
-{
+{ 
   //captando valor de PhotA
   int valueA = analogRead(photA);
   //Captando valor de PhotB
   int valueB = analogRead(photB);
-  //Serial.println(valueA);
+  //viendo en serial monitor valor de photA
+  Serial.println(valueA);
+  //viendo en serial monitor valor de photB
   //Serial.println(valueB);
-  delay(100);
-  //condicional para activar actuadores
-  if (){
-  	
-  }else{
-  	
+  //condicional para activar servo motor
+  if (valueA>activarServo && valueB>activarServo){
+    servoMotor.write(90);
+    delay(100);
+  }
+  //condicional para llevar el servo motor a la posicion original
+  if(
+    
   }
 }
